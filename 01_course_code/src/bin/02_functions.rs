972a317a0f99e633db3a2c477666313ce061ca97
danielOuattara <daniel.ouattara@gmx.com>
2023-05-08 23:04:03 +0200

    function

---
+++ 01_course_code/src/bin/02_functions.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let greetings = say_hello("Daniel");
+    println!("{greetings}");
+    //-------------------------------------------
+
+    let x = add(1, 1);
+    println!("x  =  {}", x)
+    // println! {"x = {}", add(1,2)}
+
+    //-------------------------------------------
+
+    // println!("{}", x);
+    // let y = add(3, 0);
+    // let z = add(x, 1);
+}
+
+fn say_hello(name: String) -> String {
+    "Hello ".to_owned() + &name;
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
