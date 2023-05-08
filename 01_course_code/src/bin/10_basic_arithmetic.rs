9da13aed68df69decca4a0b3ddb4a8a1765754b3
danielOuattara <daniel.ouattara@gmx.com>
2023-05-08 23:06:24 +0200

    basic arithmetics

---
+++ 01_course_code/src/bin/10_basic_arithmetic.rs
@@ -0,0 +1,24 @@
+fn subtractor(number1: i32, number2: i32) -> i32 {
+    number1 - number2
+}
+
+fn main() {
+    let summation = 2 + 3;
+    println!("2 + 3 = {}", summation);
+
+    let subtraction = 10 - 5;
+    println!("10 - 5 = {}", subtraction);
+
+    let division = 10 / 2;
+    println!("10 / 2 = {}", division);
+
+    let multiplication = 5 * 10;
+    println!("5 * 10 = {}", multiplication);
+
+    let sub_result = subtractor(8, 3);
+
+    println!("sub_result = {}", sub_result);
+
+    let modulo = 20 % 3;
+    println!("20 % 3 = {} ", modulo);
+}
