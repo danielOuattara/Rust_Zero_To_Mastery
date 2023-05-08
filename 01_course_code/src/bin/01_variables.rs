98d249d671e7b0e05bb7d41fc8179ed6d81fafd7
danielOuattara <daniel.ouattara@gmx.com>
2023-05-08 23:03:54 +0200

    variables

---
+++ 01_course_code/src/bin/01_variables.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let _two = 2;
+    let _hello = "hello";
+    let _j = "j";
+    let _my_half = 0.5;
+    let mut _my_name = "Bill";
+    let _quit_program = false;
+    let _your_half = _my_half
+}
