9d82b41e0eca548808b73a6c044197dc36d50ab0
danielOuattara <daniel.ouattara@gmx.com>
2023-05-08 23:05:30 +0200

    println macro

---
+++ 01_course_code/src/bin/03_println_macro.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let life = 42;
+
+    println!("Hello");
+    println!("{}", life);
+    println!("{:?}", life);
+    println!("{:?} {}", life, life);
+    println!("The meaning is {:?}", life);
+
+    //
+
+    println!("{life:?}");
+    println!("{life}");
+
+    /*
+
+    {:?}
+
+    {varname:?}
+
+     */
+}
