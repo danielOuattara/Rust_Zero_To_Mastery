7ead5e29a8cf6fe84f8d09d50244aeb29b21b3fd
danielOuattara <daniel.ouattara@gmx.com>
2023-05-10 23:42:45 +0200

    19 memory intermediate

---
+++ 01_course_code/src/bin/19_memory_intermediate.rs
@@ -0,0 +1,25 @@
+/*
+
+BASIC memory refresh
+---------------------
+° Memory is built using binary: bits =  0 OR 1
+° computer optimized for bytes: 1 byte = 8 contiguous bits
+° Memory fully contiguous
+
+ADDRESSES
+----------
+ ° All data in memory has an "address":
+    - used to locate data
+    - address is always the same, only data at this address changes
+
+ ° Usually don't utilize addresses directly:
+    - variables handle most of the work
+
+
+OFFSETS
+---------
+° Items can be located at an address using an "offset"
+° Offsets begin at 0
+° Represent the number of bytes away from the original address:
+    - normally deal with indexes instead
+ */
