f81e2bc1c98c86c3d8ae458052b37c9256733677
danielOuattara <daniel.ouattara@gmx.com>
2023-05-11 00:38:52 +0200

    22 vectors

---
+++ 01_course_code/src/bin/22_vectors.rs
@@ -0,0 +1,52 @@
+/*
+
+=== VECTOR ===
+
+° Multiple pieces of data
+    - must be the same type
+
+° Used for lists of information
+
+° Can add, remove and traverse the entries
+
+*/
+
+fn main() {
+    let my_numbers = vec![1, 2, 3];
+    println!("{:?}", my_numbers);
+
+    let mut my_numbers = Vec::new();
+    my_numbers.push(1);
+    my_numbers.push(2);
+    my_numbers.push(3);
+    my_numbers.pop();
+    my_numbers.len(); // => 2
+    println!("{:?}", my_numbers);
+
+    let two = my_numbers[1];
+    println!("{:?}", two);
+
+    let _my_numbers_2 = Vec::<i32>::new();
+
+    let my_numbers = vec![1, 2, 3];
+    for number in my_numbers {
+        println!("{:?}", number);
+    }
+
+    let my_scores = vec![
+        Test { score: 90 },
+        Test { score: 88 },
+        Test { score: 77 },
+        Test { score: 93 },
+    ];
+
+    for test in my_scores {
+        println!("{:?}", test.score);
+    }
+}
+
+// DEMO
+
+struct Test {
+    score: i32,
+}
