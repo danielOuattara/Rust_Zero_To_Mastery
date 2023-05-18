fb4d2d83ffff2a9ea5046031c67db58a9ba2b7d5
danielOuattara <daniel.ouattara@gmx.com>
2023-05-18 23:20:56 +0200

    course : correcting function logic

--- 01_course_code/src/bin/02_functions.rs
+++ 01_course_code/src/bin/02_functions.rs
@@ -1,6 +1,6 @@
 fn main() {
     let greetings = say_hello("Daniel");
-    println!("{greetings}");
+    println!("{:?}", greetings);
     //-------------------------------------------
 
     let x = add(1, 1);
@@ -14,8 +14,8 @@ fn main() {
     // let z = add(x, 1);
 }
 
-fn say_hello(name: String) -> String {
-    "Hello ".to_owned() + &name;
+fn say_hello(name: &str) -> str {
+    "Hello ".to_owned() + name;
 }
 
 fn add(a: i32, b: i32) -> i32 {
