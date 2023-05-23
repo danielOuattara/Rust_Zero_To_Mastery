4e44cc7abd4266945f415984d4d5aa8f00c01f22
danielOuattara <daniel.ouattara@gmx.com>
2023-05-23 21:09:10 +0200

    course: 26 enum augmented

---
+++ 01_course_code/src/bin/26_enum_augmented.rs
@@ -0,0 +1,25 @@
+enum Mouse {
+    LeftClick,
+    RightClick,
+    MiddleClick,
+    Scroll(i32),
+    Move(i32, i32),
+}
+
+enum PromoDiscount {
+    NewUser,
+    Holiday(String),
+}
+
+enum Discount {
+    Percent(f64),
+    Flat(i32),
+    Promo(PromoDiscount),
+    Custom(String),
+}
+
+fn main() {
+    where_to(Direction::Up);
+}
+
+/*  STOPPED  4H20'  */
