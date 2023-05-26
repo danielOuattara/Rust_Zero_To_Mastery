b90512c900c9ef1c4a176ecca7375feb7a40b844
danielOuattara <daniel.ouattara@gmx.com>
2023-05-26 21:49:32 +0200

    course: 30 demo fro Rust documentation

---
+++ 01_course_code/src/bin/30_demo_documentation.rs
@@ -0,0 +1,19 @@
+/// A favorite color
+enum Color {
+    Red,
+    Blue,
+}
+
+/// A piece of email
+struct Mail {
+    address: String,
+}
+
+/// Adds 2 numbers together
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {}
+
+// the run in terminal: "cargo doc --open" to run and see the docs
