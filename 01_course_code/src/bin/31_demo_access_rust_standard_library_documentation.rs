fdc3b25046c042d1f37903b12732b79014d672c0
danielOuattara <daniel.ouattara@gmx.com>
2023-06-01 09:11:51 +0200

    course: 31 demo access rust std library help

---
+++ 01_course_code/src/bin/31_demo_access_rust_standard_library_documentation.rs
@@ -0,0 +1,14 @@
+// check if a vector is empty
+
+fn main() {
+    let numbers = vec![1, 2, 3];
+
+    match numbers.is_empty() {
+        true => println!("No items found in vector"),
+        false => println!("Has items in vector"),
+    }
+}
+
+// to access the Rust doc: rustup doc
+
+// activity @17
