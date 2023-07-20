9d6782ab0341219339c0aa90526e6c07fc6d961e
danielOuattara <daniel.ouattara@gmx.com>
2023-07-20 12:21:51 +0200

    16 Struct logic updated

--- 01_course_code/src/bin/16_struct.rs
+++ 01_course_code/src/bin/16_struct.rs
@@ -4,26 +4,28 @@ struct ShippingBox {
     height: u8,
 }
 
-//------------------------------
-struct GroceryItem {
-    stock: u16,
-    price: f32, // double
-}
-
-//------------------------------
-fn main() {
+fn shipping_box() {
     let my_box_1: ShippingBox = ShippingBox {
         depth: 3,
         width: 2,
         height: 5,
     };
 
-    let height: u8 = my_box_1.height;
+    let height = my_box_1.height;
     println!("The box is {:?} units tall", height);
     println!("The box is {:?} units tall", my_box_1.height);
+    println!("The box is {:?} units height", my_box_1.height);
+    println!("----------------------------------")
+}
 
-    //--------------------------------------------
+//-----------------------------------------------------------------
+// Demo
+struct GroceryItem {
+    stock: u16,
+    price: f32, // double
+}
 
+fn grocery_stock() {
     let cereal: GroceryItem = GroceryItem {
         stock: 10,
         price: 2.99,
@@ -31,4 +33,11 @@ fn main() {
 
     println!("stock in cereal: {:?}", cereal.stock);
     println!("price in cereal: {:?}", cereal.price);
+    println!("----------------------------------")
+}
+
+//-----------------------------------------------------------------
+fn main() {
+    shipping_box();
+    grocery_stock();
 }
