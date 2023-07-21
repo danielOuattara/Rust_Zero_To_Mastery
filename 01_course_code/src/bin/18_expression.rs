77850cbab1d4e59e421339e81469e4118a00e0e1
danielOuattara <daniel.ouattara@gmx.com>
2023-07-21 23:29:28 +0200

    lecture 18 expression logic update

--- 01_course_code/src/bin/18_expression.rs
+++ 01_course_code/src/bin/18_expression.rs
@@ -19,27 +19,29 @@ enum Access {
 }
 
 fn main() {
-    // Example 1
+    // --------------------------- Example 1
     let _my_num = 3;
 
-    let _is_let_5 = if _my_num < 5 { true } else { false };
-    println!("{_is_let_5}");
+    let is_less_than_5 = if _my_num < 5 { true } else { false };
+    println!("{is_less_than_5}");
 
-    let _is_let_5 = _my_num > 5; // shortcut
-    println!("{_is_let_5}");
+    // shortcut
+    let is_less_than_5 = _my_num > 5;
+    println!("{is_less_than_5}");
 
-    // Example 2
+    // ---------------------------- Example 2
     let _my_num = 3;
-    let _message = match _my_num {
+    let message = match _my_num {
         1 => "hello",
         _ => "goodbye",
     };
-    println!("{_message}");
+    println!("{message}"); // goodbye
 
-    // Example 3
+    // ---------------------------- Example 3
     let paid = true;
     let item = Menu::Drink;
     let drink_type = "Water";
+
     let order_placed = match item {
         Menu::Drink => {
             if drink_type == "Water" {
@@ -51,9 +53,11 @@ fn main() {
         _ => true,
     };
 
-    // secret file; admin only
+    // Demo
+    // secret file: admin only
 
     let access_level = Access::Guest;
+
     let can_access_file = match access_level {
         Access::Admin => true,
         _ => false,
